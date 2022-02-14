0bcfe40d16127149eba77a67016f40c560d53f01
Ryu <goskn88006@yahoo.co.jp>
2022-02-14 22:19:34 +0900

    change the routes for letter_opener

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show,:edit]
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine => '/letter_opener'
 end
