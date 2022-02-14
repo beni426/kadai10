a563ece9eb23e92c4e363e82ebcdadb5447dbb36
Ryu <goskn88006@yahoo.co.jp>
2022-02-14 19:22:25 +0900

    letter_opener機能herokuに搭載

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,18 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'ancient-bayou-35708.herokuapp.com'}
- config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :letter_opener_web
+  
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
 
 
